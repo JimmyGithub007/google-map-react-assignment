d432bc42f684ccfb66aa434104cc8560de56a8d8
jimmyGithub007 <jimmygithub007@gmail.com>
2023-03-02 11:07:08 +0800

    Moving all styled to Global.js - 2

--- src/App.js
+++ src/App.js
@@ -1,88 +1,15 @@
-import { Badge, Drawer, Fab, Grid, Grow, IconButton, Typography } from "@mui/material";
+import { Badge, Grid, Grow, IconButton, Typography } from "@mui/material";
 import { Close, HistoryOutlined, HistoryRounded, MapOutlined } from "@mui/icons-material";
+import { MuiDrawer, MuiFloatButton, MuiShadowTypography, MuiTypography } from "./styles/Global";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
-import styled from "@emotion/styled";
 import GoogleMap from "./components/GoogleMap";
 import History from "./components/History";
 import PlacesAutoComplete from "./components/PlacesAutoComplete";
 import withGoogleMaps from "./hoc/withGoogleMaps";
-import './App.css';
-import { useState } from "react";
 
-const MuiShadowTypography = styled("div")({
-  display: "flex",
-  justifyContent: "center",
-  position: "relative",
-  textAlign: "center",
-  width: "100%",
-  '.MuiTypography-root:nth-of-type(2)': {
-    alignItems: "center",
-    color: "#e8eaed82",
-    display: "flex",
-    fontSize: "25px",
-    fontWeight: "900",
-    position: "absolute",
-    top: "8px",
-    zIndex: "-1",
-    'svg': {
-      fontSize: "inherit",
-    },
-    '@media (min-width: 480px)': {
-      fontSize: "40px"
-    },
-    '@media (min-width: 768px)': {
-      fontSize: "45px"
-    }
-  }
-});
-
-const MuiTypography = styled(Typography)({
-  fontSize: "25px",
-  '@media (min-width: 480px)': {
-    fontSize: "30px"
-  },
-  '@media (min-width: 768px)': {
-    fontSize: "35px"
-  },
-  'b:nth-of-type(1), b:nth-of-type(4), b:nth-of-type(8), b:nth-of-type(11)': {
-    color: "#4285F4"
-  },
-  'b:nth-of-type(2), b:nth-of-type(6), b:nth-of-type(9), b:nth-of-type(13)': {
-    color: "#EA4335"
-  },
-  'b:nth-of-type(3), b:nth-of-type(10)': {
-    color: "#FBBC05"
-  },
-  'b:nth-of-type(5), b:nth-of-type(12)': {
-    color: "#34A853"
-  },
-  'b:nth-of-type(7), b:nth-of-type(14)': {
-    color: "#767676"
-  }
-});
-
-const MuiFloatButton = styled(Fab)({
-  position: 'absolute',
-  bottom: 16,
-  right: 16,
-  '.MuiBadge-badge': {
-    top: "-6px",
-    left: "-5px",
-    height: "25px",
-    width: "25px",
-    borderRadius: "100%",
-  }
-});
-
-const MuiDrawer = styled(Drawer)({
-  '.MuiDrawer-paper': {
-    width: "100vw",
-    '@media (min-width: 576px)': {
-      width: "500px"
-    }
-  }
-});
+import './styles/App.css';
 
 const App = () => {
   const { history } = useSelector(state => state.places);
--- src/styles/Global.js
+++ src/styles/Global.js
@@ -1,5 +1,14 @@
-import { Card, CircularProgress, TextField } from "@mui/material";
+import { Card, CircularProgress, Drawer, Fab, TextField, Typography } from "@mui/material";
 import styled from "@emotion/styled";
+
+const colors = {
+    gBlue : "#4285F4",
+    gRed: "#EA4335",
+    gYellow: "#FBBC05",
+    gGreen: "#34A853",
+    gGray: "#767676",
+    sGray: "#e8eaed82"
+};
 
 export const MuiAutoCompleteField = styled(TextField)({
     width: "100%",
@@ -20,6 +29,28 @@ export const MuiDiv = styled("div")({
     padding: "0 20px"
 });
 
+export const MuiDrawer = styled(Drawer)({
+    '.MuiDrawer-paper': {
+        width: "100vw",
+        '@media (min-width: 576px)': {
+            width: "500px"
+        }
+    }
+});
+
+export const MuiFloatButton = styled(Fab)({
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+    '.MuiBadge-badge': {
+        top: "-6px",
+        left: "-5px",
+        height: "25px",
+        width: "25px",
+        borderRadius: "100%",
+    }
+});
+
 export const MuiProgress = styled(CircularProgress)({
     height: "40px",
     position: "absolute",
@@ -28,8 +59,60 @@ export const MuiProgress = styled(CircularProgress)({
     width: "40px",
 });
 
+export const MuiShadowTypography = styled("div")({
+    display: "flex",
+    justifyContent: "center",
+    position: "relative",
+    textAlign: "center",
+    width: "100%",
+    '.MuiTypography-root:nth-of-type(2)': {
+        alignItems: "center",
+        color: colors.sGray,
+        display: "flex",
+        fontSize: "25px",
+        fontWeight: "900",
+        position: "absolute",
+        top: "8px",
+        zIndex: "-1",
+        'svg': {
+            fontSize: "inherit",
+        },
+        '@media (min-width: 480px)': {
+            fontSize: "40px"
+        },
+        '@media (min-width: 768px)': {
+            fontSize: "45px"
+        }
+    }
+});
+
 export const MuiSpan = styled("span")({
     display: "flex",
     alignItems: "center",
     gap: "5px",
+});
+
+export const MuiTypography = styled(Typography)({
+    fontSize: "25px",
+    '@media (min-width: 480px)': {
+        fontSize: "30px"
+    },
+    '@media (min-width: 768px)': {
+        fontSize: "35px"
+    },
+    'b:nth-of-type(1), b:nth-of-type(4), b:nth-of-type(8), b:nth-of-type(11)': {
+        color: colors.gBlue
+    },
+    'b:nth-of-type(2), b:nth-of-type(6), b:nth-of-type(9), b:nth-of-type(13)': {
+        color: colors.gRed
+    },
+    'b:nth-of-type(3), b:nth-of-type(10)': {
+        color: colors.gYellow
+    },
+    'b:nth-of-type(5), b:nth-of-type(12)': {
+        color: colors.gGreen
+    },
+    'b:nth-of-type(7), b:nth-of-type(14)': {
+        color: colors.gGray
+    }
 });
